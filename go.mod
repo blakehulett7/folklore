2dad8147fb26125f5f46b6d34db5f46259b51aec
blakehulett7 <blake.hulett7@gmail.com>
2024-09-11 14:02:29 -0500

    Gotta get this menu library first

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/blakehulett7/FolkloreClient
+
+go 1.22.3
--- main.go
+++ main.go
@@ -1,1 +1,10 @@
 package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Christ is King!")
+	fmt.Println("\nWelcome to Folklore!")
+}
