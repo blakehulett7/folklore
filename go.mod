cf427e6d1e8db7e1eaf0a0e0fa2370bd405c41bd
blakehulett7 <blake.hulett7@gmail.com>
2024-09-12 18:21:04 -0500

    Menu seems to be working great!

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/blakehulett7/FolkloreClient
 
 go 1.22.3
+
+require github.com/blakehulett7/goToYourMenu v0.0.0-20240912212502-22119d069559 // indirect
--- main.go
+++ main.go
@@ -2,9 +2,32 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/blakehulett7/goToYourMenu"
 )
 
+var startMenuOptions = []goToYourMenu.MenuOption{
+	{
+		Name:    "Login",
+		Command: func() { fmt.Println("Login") },
+	},
+	{
+		Name:    "Create Account",
+		Command: func() { fmt.Println("Create") },
+	},
+}
+
 func main() {
+	Run("clear")
 	fmt.Println("Christ is King!")
 	fmt.Println("\nWelcome to Folklore!")
+	goToYourMenu.Menu(startMenuOptions)
+}
+
+func Run(program string, args ...string) {
+	command := exec.Command(program, args...)
+	command.Stdout = os.Stdout
+	command.Run()
 }
